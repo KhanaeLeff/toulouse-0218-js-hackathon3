7da63bef1883d24933c5fb6ea876c7827de6f808
KhanaeLeff <scenesdecrime@gmail.com>
2018-06-14 15:47:39 +0200

    balls added

--- front/src/FullPage.js
+++ front/src/FullPage.js
@@ -2,20 +2,37 @@ import React from 'react'
 import {FullPage, Slide} from 'react-full-page'
 import {Grid, Image} from 'semantic-ui-react'
 import terre from './images/terre.png'
+import WinNews from './images/Win_News.png'
+import Boules from './images/traitboules.png'
 
+let options= {
+  arrowNavigation: true,
+  navigation: true,
+
+}
 class PageSlide extends React.Component {
   render () {
     return (
       <FullPage>
-        <Slide className="pageun">
+        <Slide options className="pageun">
+          <Grid column='three'>
+            <Grid.Row verticalAlign='middle'>
+              <Grid.Column>
+                <Image className="winNews" src={WinNews} centered></Image>
+                <Image className="boules" src={Boules} centered></Image>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         </Slide>
+
         <Slide className="pagedeux">
           <Grid column='one'>
             <Grid.Column>
-              <Image src={terre} centered alt="pourquoi ça marche pas ?"></Image>
+              <Image className="earth" src={terre} centered alt="pourquoi ça marche pas ?"></Image>
             </Grid.Column>
           </Grid>
         </Slide>
+
         <Slide className="pagetrois">
         </Slide>
       </FullPage>
